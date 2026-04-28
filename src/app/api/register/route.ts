2240bf22b736cc799591071d2fd7292acdbf72bd
antar roy <antar.ampit@gmail.com>
2026-04-28 13:54:53 +0530

    pinkay

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma"; 
 
 type RegisterPayload = {
   name?: string;
